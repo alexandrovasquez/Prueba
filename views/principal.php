b9bf65d386307c0d9b1a9c4582ff26a9870f8524
alexandrovasquez <96029461+alexandrovasquez@users.noreply.github.com>
2021-12-13 02:09:28 -0500

    iee

---
+++ views/principal.php
@@ -0,0 +1,35 @@
+﻿    <?php
+    session_start();
+    ?>
+
+    <html>
+    	    <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+            <title>SISTEMA DE MATRICULA</title>
+        </head>
+        <body>
+            <nav class="navbar navbar-expand-lg navbar-light bg-danger">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <input type="submit" align="rigth" value="INICIO" class="btn btn-danger" onclick = "location='../../views/principal.php/'"/>
+                        <input type="submit" align="rigth" value="ALUMNO" class="btn btn-danger" onclick = "location='../../controllers/alumno.php/'"/>
+                        <input type="submit" align="rigth" value="APODERADO" class="btn btn-danger" onclick = "location='../../controllers/apoderado.php/'"/>
+	  <input type="submit" align="rigth" value="PAGO" class="btn btn-danger" onclick = "location='../../controllers/pago.php/'"/>
+	   <input type="submit" align="rigth" value="RESULTADOS" class="btn btn-danger" onclick = "location='../../controllers/resultado.php/'"/>
+                    </ul>
+
+    <input type="submit" style="margin-left:850px; border:none" align="rigth" value="SALIR" class="btn btn-danger" onclick = "location='../../index.php/'"/>
+                   
+                    </div>
+                </div>
+
+            </nav>
+    <img src="../../imagenes/IEE_01.jpg"/ width=100% height=80%/>
+    <div class="footer-copyright text-center py-3">INGENIERÍA DE SISTEMAS E INFORMÁTICA - PROMOCIÓN 2017
+            <br>
+            <a href="https://www.uns.edu.pe/" target="_blank"> UNIVERSIDAD NACIONAL DEL SANTA</a>
+          </div>
+
+    	</body>
+    </html>
